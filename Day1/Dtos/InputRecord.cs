4eec32279eeec27340a80e69ec733625c95b0257
realsonic <realsonic@yandex.ru>
2022-12-01 20:53:18 +0300

    Day 1 DDD unit-test and DTOs.

---
+++ Day1/Dtos/InputRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace Day1.Dtos;
+
+public record InputRecord()
+{
+    public static InputRecord BuildFromString(string record)
+        => !string.IsNullOrEmpty(record)
+            ? new CaloriesRecord(uint.Parse(record))
+            : new DelimiterRecord();
+};
+
+public record CaloriesRecord(uint Calories) : InputRecord;
+
+public record DelimiterRecord() : InputRecord;
--- ExamplesTests/Day1/ExamplesTests.cs
+++ ExamplesTests/Day1/ExamplesTests.cs
@@ -1,4 +1,6 @@
 using Day1;
+using Day1.Dtos;
+using Day1.Model;
 
 namespace ExamplesTests.Day1;
 
@@ -31,4 +33,49 @@ public class ExamplesTests
     }
 
     private readonly Solution solution = new(File.ReadLinesAsync(@"Day1\example.input.txt"));
+
+    [Fact]
+    public async Task Test_Expedition()
+    {
+        //Arrange
+        uint puzzle1Result = uint.Parse(await File.ReadAllTextAsync(@"Day1\example.output1.txt"));
+        Expedition expedition = await Expedition.BuildExpeditionAsync(GetElvesAsync());
+
+        //Act
+        CaloriesValue maxCaloriesPerElf = await expedition.MaxCaloriesPerElf;
+
+        //Assert
+        Assert.Equal(puzzle1Result, maxCaloriesPerElf);
+    }
+
+    private static async IAsyncEnumerable<Elf> GetElvesAsync()
+    {
+        List<Snack> snacks = new();
+
+        await foreach (var inputRecord in GetInputRecordsAsync())
+        {
+            if (inputRecord is CaloriesRecord caloriesRecord)
+            {
+                snacks.Add(new Snack(caloriesRecord.Calories));
+            }
+            else if (inputRecord is DelimiterRecord)
+            {
+                yield return new Elf(snacks);
+                snacks.Clear();
+            }
+        }
+
+        if (snacks.Count > 0)
+        {
+            yield return new Elf(snacks);
+        }
+    }
+
+    private static async IAsyncEnumerable<InputRecord> GetInputRecordsAsync()
+    {
+        await foreach (string record in File.ReadLinesAsync(@"Day1\example.input.txt"))
+            yield return InputRecord.BuildFromString(record);
+
+        yield return new DelimiterRecord();
+    }
 }
