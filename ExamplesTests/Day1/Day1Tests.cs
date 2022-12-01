bc3e05d74adbf575eb78136183305e449b9ed699
realsonic <realsonic@yandex.ru>
2022-12-01 17:24:27 +0300

    Day 1 added unit-tests.

---
+++ ExamplesTests/Day1/Day1Tests.cs
@@ -0,0 +1,39 @@
+using Day1;
+
+namespace ExamplesTests.Day1;
+
+public class Day1Tests
+{
+    public Day1Tests()
+    {
+        solution = new Solution(File.ReadLinesAsync(@"Day1\example.input.txt"));
+    }
+
+    [Fact]
+    public async Task TestPuzzle1Async()
+    {
+        //Arrange
+        int puzzle1Result = int.Parse(await File.ReadAllTextAsync(@"Day1\example.output1.txt"));
+
+        //Act
+        int maxCaloriesPerElf = await solution.MaxCaloriesPerElf;
+
+        //Assert
+        Assert.Equal(puzzle1Result, maxCaloriesPerElf);
+    }
+
+    [Fact]
+    public async Task TestPuzzle2Async()
+    {
+        //Arrange
+        int puzzle2Result = int.Parse(await File.ReadAllTextAsync(@"Day1\example.output2.txt"));
+
+        //Act
+        int totalCaloriesFromTop3Elves = await solution.TotalCaloriesFromTop3Elves;
+
+        //Assert
+        Assert.Equal(puzzle2Result, totalCaloriesFromTop3Elves);
+    }
+
+    private readonly Solution solution;
+}
